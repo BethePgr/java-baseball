3fe6db293a2c94df646bb9c35faada3ea58fae52
SEC <c3392302@naver.com>
2023-04-10 13:25:09 +0900

    fix : Input 종료 시 문구 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,9 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    private static String RESTART_MESSAGE = "게임을 다시 시작하시겠습니까?";
+    private static String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public static String inputUserNumber(){
-        System.out.println(INPUT_NUMBER);
+        System.out.print(INPUT_NUMBER);
         return Console.readLine();
     }
 
