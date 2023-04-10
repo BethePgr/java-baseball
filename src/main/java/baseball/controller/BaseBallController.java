e8550e7e2a4732d76c33b0fbdf1affb336604947
SEC <c3392302@naver.com>
2023-04-10 14:17:20 +0900

    feat : baseballservice를 이용해 구현된 최종적인 프로그램인 BaseBallController

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -3,38 +3,38 @@ package baseball.controller;
 import baseball.domain.ComputerNumber;
 import baseball.domain.GameResult;
 import baseball.domain.UserNumber;
+import baseball.service.BaseBallService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BaseBallController {
 
-    public void run(){
+    private BaseBallService baseBallService;
+
+    public BaseBallController(){
         OutputView.printStartMessage();
+    }
+
+    public void run(){
         boolean flag = false;
         do{
+            this.baseBallService = new BaseBallService();
             runOneCycle();
             OutputView.printClearMessage();
-            flag = isRestartGame(InputView.inputRestartNumber());
+            flag = baseBallService.wantRestart(InputView.inputRestartNumber());
         }while(flag);
 
     }
 
     private void runOneCycle(){
         int strikeCount = 0;
-        List<Integer> computerNumber = new ComputerNumber().getComputerNumber();
-        while(strikeCount != computerNumber.size()) {
-            List<Integer> userNumber = new UserNumber(InputView.inputUserNumber()).getUserNumber();
-            GameResult gameResult = new GameResult(computerNumber, userNumber);
+        baseBallService.initBaseBallGame();
+        while(strikeCount != 3){
+            GameResult gameResult = baseBallService.countBallStrike(InputView.inputUserNumber());
             strikeCount = gameResult.strikeCount;
             new OutputView(gameResult).printResult();
         }
     }
 
-    private boolean isRestartGame(String str) throws IllegalArgumentException{
-        if (str.equals("1")) return true;
-        if (str.equals("2")) return false;
-        throw new IllegalArgumentException();
-    }
 }
