72f59ceab5ff6c0990a932b176c776b01a977c13
SEC <c3392302@naver.com>
2023-04-10 12:33:08 +0900

    feat: 게임 결과에 따른 출력 메시지 구현 완료

---
+++ src/test/java/baseball/view/OutputView.java
@@ -0,0 +1,57 @@
+package baseball.view;
+
+import baseball.domain.GameResult;
+
+public class OutputView {
+
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!게임 종료";
+    private final GameResult gameResult;
+
+    public OutputView(GameResult gameResult){
+        this.gameResult = gameResult;
+    }
+
+    public static void printStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printClearMessage(){
+        System.out.println(CLEAR_MESSAGE);
+    }
+
+    public void printResult(){
+        printOnlyBall();
+        printOnlyStrike();
+        printBallAndStrike();
+        printNothing();
+    }
+
+
+    private void printOnlyBall(){
+        if(gameResult.ballCount != 0 && gameResult.strikeCount == 0){
+            System.out.println(gameResult.ballCount+"볼");
+        }
+    }
+
+    private void printOnlyStrike(){
+        if(gameResult.ballCount == 0 && gameResult.strikeCount !=0){
+            System.out.println(gameResult.strikeCount+"스트라이크");
+        }
+    }
+
+    private void printBallAndStrike(){
+        if(gameResult.ballCount != 0 && gameResult.strikeCount !=0){
+            System.out.println(gameResult.ballCount+"볼 " + gameResult.strikeCount+"스트라이크");
+        }
+    }
+
+    private void printNothing(){
+        if(gameResult.ballCount == 0 && gameResult.strikeCount == 0){
+            System.out.println("낫싱");
+        }
+    }
+
+
+
+}
