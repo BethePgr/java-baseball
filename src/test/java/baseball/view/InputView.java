1206c0f6b6f5497d466abfcbba794cffb101789d
SEC <c3392302@naver.com>
2023-04-10 12:42:12 +0900

    feat: 게임 재시작 input문 추가

--- src/test/java/baseball/view/InputView.java
+++ src/test/java/baseball/view/InputView.java
@@ -5,10 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-
+    private static String RESTART_MESSAGE = "게임을 다시 시작하시겠습니까?";
     public static String inputUserNumber(){
         System.out.println(INPUT_NUMBER);
         return Console.readLine();
     }
 
+    public static String inputRestartNumber(){
+        System.out.println(RESTART_MESSAGE);
+        return Console.readLine();
+    }
 }
