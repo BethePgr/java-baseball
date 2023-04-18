e7fd1d08482594b2c7bb566c5431a4f23fdb1fd3
SEC <c3392302@naver.com>
2023-04-18 13:38:19 +0900

    test: 세컨 브랜치

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,9 @@ import baseball.controller.BaseBallController;
 
 public class Application {
 
+    private String second = "b";
+    private String secon2 = "b";
+
     public static void main(String[] args) {
         BaseBallController game = new BaseBallController();
         game.run();
