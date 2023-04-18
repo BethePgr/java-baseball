6ccd3b782d619bbbd830440e8edfcc0067ea60f2
SEC <c3392302@naver.com>
2023-04-18 13:40:59 +0900

    second 브랜치에 재시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,7 @@
 package baseball;
 
-import baseball.controller.BaseBallController;
-
 public class Application {
-
-    private String second = "b";
-    private String secon2 = "b";
-
     public static void main(String[] args) {
-        BaseBallController game = new BaseBallController();
-        game.run();
+        // TODO: 프로그램 구현
     }
 }
