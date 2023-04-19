a7b503a3018c901eb985ec36fced46d11f2c3627
jongwonpc <c3392302@naver.com>
2023-04-20 06:09:54 +0900

    feat: 재시작시 사용자로부터 입력 받기

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,9 +7,14 @@ import java.util.Arrays;
 public class InputView {
 
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-
+    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public String startMessage(){
         System.out.print(INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String reStartMessage(){
+        System.out.println(INPUT_RESTART_MESSAGE);
         return Console.readLine();
     }
 
