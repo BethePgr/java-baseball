b35e58c39a92bc2d6b3a06c22cc834cec1e635df
jongwonpc <c3392302@naver.com>
2023-04-20 07:15:29 +0900

    refactor: 게임 시작 메세지 리팩토링

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -11,7 +11,7 @@ public class BaseBallController {
     private BaseBallService baseBallService;
 
     public void run() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        OutputView.printStart();
         String input = "";
         do {
             runOneCycle();
