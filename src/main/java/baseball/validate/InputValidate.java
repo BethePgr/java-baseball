f4132aa9d233f84348d28f44d1fee28ca9151c9f
jongwonpc <c3392302@naver.com>
2023-04-20 06:45:45 +0900

    refactor: 숫자 입력 검증 리팩토링

--- src/main/java/baseball/validate/InputValidate.java
+++ src/main/java/baseball/validate/InputValidate.java
@@ -2,9 +2,9 @@ package baseball.validate;
 
 public class InputValidate {
 
-    public void validateInputNumber(String input) {
+    public boolean validateInputNumber(String input) {
         if(validateDistinct(input) && validateSize(input) && validateOnlyNumber(input)){
-            return;
+            return true;
         }
         throw new IllegalArgumentException("[ERROR] 중복되지 않은 3자리 숫자만 입력 가능합니다.");
     }
@@ -21,9 +21,9 @@ public class InputValidate {
         return input.chars().allMatch(num -> num-'0'>=1 && num-'0'<=9);
     }
 
-    public void validateInputRestart(String input){
+    public boolean validateInputRestart(String input){
         if(input.equals("1") || input.equals("2")){
-            return;
+            return true;
         }
         throw new IllegalArgumentException("[ERROR] 재시작을 원하는 1이나 종료를 원하는 2밖에 입력이 불가능합니다.");
     }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,14 +8,29 @@ public class InputView {
 
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final InputValidate inputValidate = new InputValidate();
     public String startMessage(){
         System.out.print(INPUT_NUMBER_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        try{
+            inputValidate.validateInputNumber(input);
+            return input;
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
     public String reStartMessage(){
         System.out.println(INPUT_RESTART_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        try{
+            inputValidate.validateInputRestart(input);
+            return input;
+        }catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
 }
