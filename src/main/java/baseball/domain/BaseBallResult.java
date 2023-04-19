c6313b48cac8ec5d217c77274a96184b032ab716
jongwonpc <c3392302@naver.com>
2023-04-20 05:20:11 +0900

    feat: 볼개수와 스트라이크 개수를 세는 기능

---
+++ src/main/java/baseball/domain/BaseBallResult.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class BaseBallResult {
+
+    private final int ballCount;
+    private final int strikeCount;
+
+    public BaseBallResult(List<Integer> computerNums, List<Integer> userNums) {
+        this.ballCount = countBall(computerNums, userNums);
+        this.strikeCount = countStrike(computerNums, userNums);
+    }
+
+    private int countBall(List<Integer> computerNums, List<Integer> userNums) {
+        return (int) Stream.iterate(0, i -> i + 1).limit(computerNums.size()).filter(num ->
+            computerNums.get(num) != userNums.get(num) && computerNums.contains(userNums.get(num))
+        ).count();
+    }
+
+    private int countStrike(List<Integer> computerNums, List<Integer> userNums) {
+        return (int) Stream.iterate(0, i -> i + 1).limit(computerNums.size())
+            .filter(num -> computerNums.get(num) == userNums.get(num)).count();
+    }
+}
