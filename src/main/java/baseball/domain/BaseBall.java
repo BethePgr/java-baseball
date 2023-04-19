0dd31bd349667017cfef0947b65a6522c14750c2
jongwonpc <c3392302@naver.com>
2023-04-20 05:47:07 +0900

    feat: baseball 객체 생성

---
+++ src/main/java/baseball/domain/BaseBall.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseBall {
+
+    private final List<Integer> computerNumList;
+    private List<Integer> userNumList;
+    private BaseBallResult baseBallResult;
+
+    public BaseBall() {
+        computerNumList = new ComputerNumber().getComputerNum();
+    }
+
+    private void inputToList(String input) {
+        userNumList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            userNumList.add(input.charAt(i) - '0');
+        }
+    }
+
+    public BaseBallResult getBaseBallResult(){
+        return baseBallResult;
+    }
+}
