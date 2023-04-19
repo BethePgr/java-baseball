3626f29a81c4bf7bd6ab8e995a5c60f28a499687
jongwonpc <c3392302@naver.com>
2023-04-20 04:52:13 +0900

    feat: 컴퓨터가 서로다른 세 자리수를 자동으로 생성하는 기능

---
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import baseball.validate.ComputerNumValidate;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+
+    private ComputerNumValidate computerNumValidate = new ComputerNumValidate();
+    private List<Integer> computerNum;
+
+    private List<Integer> CreateComputerNum(){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0;i<3;i++){
+            list.add(Randoms.pickNumberInRange(1,9));
+        }
+        return list;
+    }
+
+    public void CreateDistinctComputerNum(){
+        List<Integer> tempList = CreateComputerNum();
+        if(computerNumValidate.checkAll(tempList)){
+            computerNum = tempList;
+        }
+        if(!computerNumValidate.checkAll(tempList)){
+            CreateDistinctComputerNum();
+        }
+    }
+
+    public List<Integer> getComputerNum(){
+        return computerNum;
+    }
+
+}
