db0444d6c62f77b22aaa0af006fdec139fdbdde1
jongwonpc <c3392302@naver.com>
2023-04-20 06:07:08 +0900

    feat: 게임 종료 멘트 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -41,4 +41,8 @@ public class OutputView {
         }
     }
 
+    public static void printEndGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+            + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
